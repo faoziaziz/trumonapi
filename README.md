3b1bd5160a36aa734f90632a11f92793a4aa4f82
Aziz Faozi <admin@labseni.com>
2021-05-18 19:43:06 +0700

    mantap

---
+++ README.md
@@ -0,0 +1,3 @@
+# MANTAP
+export CONNECTIONSTRING=mysql+mysqlconnector://pos_almar:@Alm4r2020@localhost/trumon
+
--- trumonapi.py
+++ trumonapi.py
@@ -1,5 +1,5 @@
 #
-#
+# export CONNECTIONSTRING=mysql://pos_almar:@Alm4r2020@localhost/trumon
 from starlette.middleware import Middleware
 from fastapi import FastAPI
 #from fastapi.middleware.cors import CORSMiddleware
@@ -8,18 +8,83 @@ from starlette.middleware.cors import CORSMiddleware
 import nest_asyncio
 from pyngrok import ngrok
 import uvicorn
+from pydantic import BaseModel
+import datetime
+import os
+from sqlalchemy import Column, ForeignKey, Integer, String, Numeric, DateTime, ForeignKey, CHAR, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+
+CONNECTIONSTRING = os.getenv('CONNECTIONSTRING')
+engine = create_engine(CONNECTIONSTRING)
+
+Base = declarative_base(engine)
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+class Transaksi(Base):
+    __tablename__ = "Transaksi"
+    __table_args__ = {"autoload": True}
+
 
 middleware = [
     Middleware(CORSMiddleware, allow_origins=["*"])
 ]
 
-app = FastAPI(middleware=middleware)
+app = FastAPI(
+    title="Trumon Api",
+    description="Just trumon simple api",
+    middleware=middleware,
+)
 
 class Makan:
     def simple(self):
         return "Mantap banget"
 
 makan = Makan()
+
+
+@app.get('/transaksi')
+async def GetTrans(DevID: str = "ALMAR0000003"):
+
+    transList = []
+
+    transaksis = session.query(Transaksi).all()
+    
+    for item in transaksis:
+        #contactType = session.query(ContactType).filter(ContactType.ContactTypeId == item.ContactTypeId).one()
+        #emails = [item.__dict__ for item in session.query(Email).filter(Email.ContactId == item.ContactId)]
+        #phoneNumbers = [item.__dict__ for item in session.query(PhoneNumber).filter(PhoneNumber.ContactId == item.ContactId)]
+
+        trans = {
+            "SeqNum": item.SeqNum,
+            "DeviceId": item.DeviceId,
+            "RefSN": item.RefSN,
+            "FileTime": item.FileTime,
+            "Nomor": item.Nomor,
+            "Tanggal": item.Tanggal,
+            "Jam": item.Jam,
+            "Nilai": item.Nilai,
+            "Pajak": item.Pajak,
+            "NilaiDanPajak": item.NilaiDanPajak,
+            "CustomField1": item.CustomField1,
+            "CustomField2": item.CustomField2,
+            "CustomField3": item.CustomField3,
+            "FlagTransfer": item.FlagTransfer,
+        }
+
+        if(item.DeviceId==DevID):
+            transList.append(trans)
+        #print(transList)
+
+    return {"data": transList}
+    
 
 @app.get('/index')
 async def home():
